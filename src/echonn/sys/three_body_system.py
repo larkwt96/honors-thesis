32444a46c3b5afc84aae82b9f986de394b128d4e
Lucas Wilson <lkwilson96@gmail.com>
2019-02-19 10:08:06 -0700

    implement n body

--- src/echonn/sys/three_body_system.py
+++ src/echonn/sys/three_body_system.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .system import DynamicalSystem
 from scipy.constants import G
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 class ThreeBodySystem(DynamicalSystem):
@@ -25,12 +26,58 @@ class ThreeBodySystem(DynamicalSystem):
     def pack_ham(self, r, p):
         return np.concatenate((r.reshape(-1), p.reshape(-1)))
 
+    def diag_multi(self, v, A):
+        """
+        v is a vector
+        A is a matrix
+
+        Returns diag(v)*A
+        """
+        return (v*A.T).T
+
+    def get_dist_matrix(self, r):
+        pass
+
+    def total_acc(self, r):
+        m = self.body_masses
+        total = []
+        dist_matrix = euclidean_distances(r)  # cdist is more accurate but slow
+        for i in range(self.body_count):
+            total.append([])
+            for j in range(i + 1, self.body_count):
+                mass = m[i]*m[j]
+                diff = r[i] - r[j]
+                dist = np.sqrt(diff**2).sum()
+                total[i].append(mass * diff / dist**3)
+        return total
+
     def fun(self, t, v):
-        r, p = self.unpack_ham(v)
-        # Just do the rename variables and differentiate. Make system the
+        # Just do the rename variables and differentiate thing. Make system the
         # Lagrangian and Hamiltonian:
         #     y1 = r (position)
         #     y2 = mr' (momentum)
-        rp = r
-        pp = p
+        #
+        # r' = diag(m) P
+        # p' = -G diag(m) sum j!=i of m_j (r_i - r_j) / |r_i - r_j|^3
+        #  where r_i is the ith row of the R matrix
+        #
+        # TODO: make this more efficient
+        r, p = self.unpack_ham(v)
+        m = self.body_masses
+
+        # position derivative
+        rp = self.diag_multi(1/m, p)
+
+        # momentum derivative
+        diff = r[:, None, :] - r  # broadcast each row to each other
+        dist = np.sqrt((diff**2).sum(axis=2))  # l2 norm of each difference
+
+        # divide diff by dist but replace div by zero with 0
+        dist_is_not_zero = dist[:, :, None] != 0
+        acc = np.divide(diff, dist[:, :, None],
+                        out=np.zeros_like(diff),
+                        where=dist_is_not_zero)
+
+        # apply rest of gravity equation
+        pp = -G * m[None, :] @ acc @ m[:, None]
         return self.pack_ham(rp, pp)
