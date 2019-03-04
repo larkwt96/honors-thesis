e0eea202218a06c0994e4590f584b45be935db00
Lucas Wilson <lkwilson96@gmail.com>
2019-03-04 13:40:25 -0700

    make dim settable

--- src/echonn/sys/system.py
+++ src/echonn/sys/system.py
@@ -3,16 +3,12 @@ from abc import ABC, abstractmethod
 
 class DynamicalSystem(ABC):
     def __init__(self, dim=1, method='RK45'):
-        self._dim = dim
+        self.dim = dim
         self._method = method
 
     @abstractmethod
     def fun(self, t, v):
         return v
-
-    @property
-    def dim(self):
-        return self._dim
 
     @property
     def method(self):
