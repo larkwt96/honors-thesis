4ffa3184bb808e4bc94052d1ccadc77cd939eb14
Lucas Wilson <lkwilson96@gmail.com>
2019-03-19 12:49:02 -0600

    add animator to exports list"

--- src/echonn/sys/__init__.py
+++ src/echonn/sys/__init__.py
@@ -4,3 +4,4 @@ from .lorenz_system import LorenzSystem
 from .circle_system import CircleSystem
 from .double_pendulum_system import DoublePendulumSystem
 from .n_body_system import NBodySystem
+from .animator import Animator
