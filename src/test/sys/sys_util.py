c0d9eb7f45476423b706b98b243c29d86d43c45e
Lucas Wilson <lkwilson96@gmail.com>
2019-03-21 18:11:00 -0600

    clear todo

--- src/test/sys/sys_util.py
+++ src/test/sys/sys_util.py
@@ -2,16 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def clearFigs():
-    # TODO: it doesn't want to close figures, and I don't know why. In the
-    # mean time, I will just clear the figure, and the non blank figure is
-    # the one you want.
-    # return  # global disable
+    # this can fail, but it's not an issue.
     for fignum in plt.get_fignums():
         plt.figure(fignum)
         plt.clf()
-    """
-    while len(plt.get_fignums()) > 0:
-        fignum = plt.get_fignums()[0]
-        plt.close(fignum)
-        plt.pause(.001) # TODO: try this?
-    """
