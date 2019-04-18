13d446520739e236ef1302331f098b03b642a3d4
Lucas Wilson <lkwilson96@gmail.com>
2019-04-17 20:28:54 -0600

    add 3bod results

--- doc/paper/scripts/AnalyzeResultsR3Body.py
+++ doc/paper/scripts/AnalyzeResultsR3Body.py
@@ -14,9 +14,11 @@ print('lce:', lce[0])
 print('ic:', run['results'].y[:, 0])
 print('tf:', run['results'].t[-1])
 
-rank_metric = np.array([test_rmse for _, cv_rmse, _, (_, _, test_rmse)
-                        in results['best model rmse']])
-ranking = np.argsort(rank_metric)
+rank_metric_cv = np.array([cv_rmse for _, cv_rmse, _, (_, _, test_rmse)
+                           in results['best model rmse']])
+rank_metric_test = np.array([test_rmse for _, cv_rmse, _, (_, _, test_rmse)
+                             in results['best model rmse']])
+ranking = np.argsort(rank_metric_cv)
 
 
 def rmse(d, y):
@@ -45,7 +47,7 @@ slvr.plot2d(runt)
 plt.scatter([-sys.alpha, sys.mu], [0, 0])
 plt.savefig(os.path.join(dir_pre, 'test_data.png'))
 
-how_many = 10
+how_many = 5
 for rank, i in enumerate(ranking[:how_many]):
     ds_test, ys_test, total_rmse = results['best model rmse'][i][3]
     print(i, total_rmse, results['params'][i])
@@ -54,8 +56,7 @@ for rank, i in enumerate(ranking[:how_many]):
                    for sub in range(ds_test.shape[0])]
 
     plt.figure()
-    plt.title('RMSE vs Lyapunov Time\nParam {} ; RMSE {}'.format(
-        results['params'][i], total_rmse))
+    plt.title('Test RMSE vs Lyapunov Time')
     t_adj = (ts_data.test_t - ts_data.test_t[0]) * lce[0]
     plt.plot(t_adj, rmse_over_t, 'o-')
     plt.plot(t_adj, np.zeros_like(t_adj))
@@ -67,7 +68,13 @@ for rank, i in enumerate(ranking[:how_many]):
     runt['results'].y = ys_test[:, [0, 2]].T
     slvr.plot2d(runt)
     plt.scatter([-sys.alpha, sys.mu], [0, 0])
+    title = 'ESN Trajectory\nParam {}\nValidation/Test RMSE {:.4}/{:.4}'
+    param_title = '(α:{}, N:{}, T0:{})'.format(*results['params'][i])
+    plt.title(title.format(param_title,
+                           rank_metric_cv[i],
+                           rank_metric_test[i]))
     name = 'rank_{}_param_{}_fit.png'.format(rank, i)
+    plt.tight_layout()
     plt.savefig(os.path.join(
         dir_pre, name))
     plt.close()
